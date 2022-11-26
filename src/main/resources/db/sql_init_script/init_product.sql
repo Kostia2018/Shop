321f89b03cdce231a262c0209525721880df37b2
Kostia <konstantinlulakov@gmail.com>
2022-11-26 18:26:19 +0300

    Create SQL init_script

---
+++ src/main/resources/db/sql_init_script/init_product.sql
@@ -0,0 +1,11 @@
+INSERT INTO products(id,price,title) VALUES
+(1,500,'car1'),
+(2,200,'car2'),
+(3,300,'car3'),
+(4,400,'car4'),
+(5,500,'car5'),
+(6,600,'car6'),
+(7,530,'motobike'),
+(8,543,'horse');
+
+ALTER SEQUENCE product_seq RESTART WITH 8
